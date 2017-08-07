ebfcea57b834082a860e864284646076933f0d06
Austin Cummings and Margaret Berry <austin@>
2017-08-07 14:03:11 -0700

    create initial html and js files, print user sentence in the console, find the first and last letter.

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// var sentence = prompt("Please enter a sentence.");
+// alert(sentence);
+// console.log(sentence);
+
+var firstLetter=sentence.charAt(0);
+alert(firstLetter);
+var lastLetter=sentence.charAt(sentence.length -1);
+alert(lastLetter);
